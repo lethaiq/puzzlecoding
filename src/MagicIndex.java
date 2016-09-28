88a5ce82bffdb502294ad4491c2e655a020cd56e
Quang Thai Le <tle17@hawk.iit.edu>
2016-09-28 03:15:19 -0500

    MagicIndex fix error in some cases

--- src/MagicIndex.java
+++ src/MagicIndex.java
@@ -12,7 +12,8 @@ public class MagicIndex {
 	
 	public static void main(String[] args)
 	{
-		int[] arr = {1, 2, 4, 4, 5, 5, 0, 10, 11, 11, 8, 8};
+//		int[] arr = {1, 2, 4, 4, 5, 5, 0, 10, 11, 11, 8, 8};
+		int[] arr = {-10, -5, 2, 2, 2, 3, 4, 7, 9, 12, 13};
 		Arrays.sort(arr);
 		System.out.println(Arrays.toString(arr));
 		getMagicIndex(arr, 0, arr.length - 1);
@@ -34,13 +35,13 @@ public class MagicIndex {
 		else if (arr[mid] > mid)
 		{
 			int diff = arr[mid] - mid;
-			if ((to - mid) >= diff) getMagicIndex(arr, mid + diff, mid + diff);
+			if ((to - mid) >= diff) getMagicIndex(arr, mid + diff, to);
 			getMagicIndex(arr, from, mid - 1);
 		}
 		else if (arr[mid] < mid) 
 		{
 			int diff = mid - arr[mid];
-			if ((mid - from) >= diff) getMagicIndex(arr, mid - diff, mid - diff);
+			if ((mid - from) >= diff) getMagicIndex(arr, from, mid - diff);
 			getMagicIndex(arr, mid + 1, to);
 		}
 	}
