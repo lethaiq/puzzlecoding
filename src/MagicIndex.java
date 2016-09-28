a213be583043e17cdd1030b487fd109c2fc4570c
Quang Thai Le <tle17@hawk.iit.edu>
2016-09-28 02:42:39 -0500

    Magic Index initial attempt

--- src/MagicIndex.java
+++ src/MagicIndex.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
 /**
  * Problem Statement (Problem 8.4 Cracking The Coding Interview 6th ed.
  * 
@@ -5,5 +8,32 @@
  *
  */
 public class MagicIndex {
-
+	private static HashSet<Integer> magic = new HashSet<Integer>();
+	
+	public static void main(String[] args)
+	{
+		int[] arr = {1, 2, 4, 5, 19, 20, 0, 10, 9};
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+		getMagicIndex(arr, 0, arr.length - 1);
+		for (int i:magic) System.out.println(i);
+		
+	}
+	
+	private static void getMagicIndex(int[] arr, int from, int to)
+	{	
+		if (to < from | from < 0 | from > arr.length - 1 | to < 0 | to > arr.length - 1){
+			return;
+		}
+		int mid = from + (to - from) / 2;
+//		System.out.println("checking index " + mid + " from " + from + " to " + to);
+		if (arr[mid] == mid)
+		{
+			magic.add(mid);
+			getMagicIndex(arr, mid + 1, to);
+			getMagicIndex(arr, from, mid - 1);
+		}
+		else if (arr[mid] > mid) getMagicIndex(arr, from, mid - 1);
+		else if (arr[mid] < mid) getMagicIndex(arr, mid + 1, to);
+	}
 }
