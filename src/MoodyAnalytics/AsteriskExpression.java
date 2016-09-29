901d3ec42e39c8649ff5800dfb1e5202eb8449c9
Quang Thai Le <tle17@hawk.iit.edu>
2016-09-28 20:59:10 -0500

    Hackerrank Moody's analytics contest. problem 3 AsteriskExpression added

--- src/MoodyAnalytics/AsteriskExpression.java
+++ src/MoodyAnalytics/AsteriskExpression.java
@@ -1,8 +1,26 @@
 package MoodyAnalytics;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class AsteriskExpression {
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int numExp = Integer.parseInt(in.readLine());
+		for(int i = 0; i < numExp; i++)
+		{
+			String expression = in.readLine();
+			long val = eval(expression);
+			if(val != 0) System.out.println(val);
+			else System.out.println("Syntax Error");
+		}
+	}
+	
+	private static long eval(String e)
 	{
 		
+		return 0;
 	}
 }
